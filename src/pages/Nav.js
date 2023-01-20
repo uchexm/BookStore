07357655a2dc6c1d4ba1ca9e6482d0c730e45558
uchexm <109084069+uchexm@users.noreply.github.com>
2023-01-20 09:09:54 +0200

    Fix linters error

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
   return (
     <div className="navContainer">
       <div className="logoContainer">
-        <h1 className="logo">Bookstore_UC</h1>
+        <h1 className="logo">Bookstore_CMS</h1>
       </div>
       <nav className="navLinks">
         <Link to="/">Books</Link>
