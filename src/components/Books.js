d5cf7abec49db0be070bb189fc164dd5cc7370e6
uchexm <109084069+uchexm@users.noreply.github.com>
2023-01-20 08:37:13 +0200

    Create components

---
+++ src/components/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import InputBooks from "./InputBooks";
+
+function Books() {
+  return (
+    <div className="mainBookContainer">
+      <button type="button">Remove</button>
+      <InputBooks />
+    </div>
+  );
+}
+
+export default Books;
