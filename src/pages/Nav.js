3e96f548df197d524857f027515d7c87224e17b1
uchexm <109084069+uchexm@users.noreply.github.com>
2023-01-24 10:47:36 +0200

    Add Categories

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -1,14 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <div className="navContainer">
       <div className="logoContainer">
-        <h1 className="logo">Bookstore_CMS</h1>
+        <h1 className="logo">
+          Bookstore_CMS
+        </h1>
       </div>
       <nav className="navLinks">
-        <Link to="/">Books</Link>
+        <Link to="/">BookList</Link>
         <Link to="/categories">Categories</Link>
       </nav>
     </div>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import bookReducer from "./Books/Books";
-import categoriesReducer from "./Categories/Categories";
+import categoriesReducer from "./categories/Categories";
 
 const rootReducer = combineReducers({
   books: bookReducer,
