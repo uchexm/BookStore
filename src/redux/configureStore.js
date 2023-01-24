0dde18abf660ef05d57f2376326759a01471bf4c
uchexm <109084069+uchexm@users.noreply.github.com>
2023-01-24 10:40:33 +0200

    Add reducers

---
+++ src/redux/configureStore.js
@@ -0,0 +1,14 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import bookReducer from "./Books/Books";
+import categoriesReducer from "./Categories/Categories";
+
+const rootReducer = combineReducers({
+  books: bookReducer,
+  categories: categoriesReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export default store;
