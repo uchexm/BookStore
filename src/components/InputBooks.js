84c6876ed099dae3c173cebfada51029e149543f
uchexm <109084069+uchexm@users.noreply.github.com>
2023-01-26 09:46:39 +0200

    update book inputs

--- src/components/InputBooks.js
+++ src/components/InputBooks.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/Book/Book';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import swal from "sweetalert";
+import { addBook } from "../redux/Book/Book";
+import "./InputBooks.css";
 
 function InputBooks() {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
@@ -14,22 +16,22 @@ function InputBooks() {
       id: uuidv4(),
       title,
       author,
-      progress: Math.floor(Math.random() * 100),
-      chapter: `Chapter ${Math.floor(Math.random() * 10)}`,
     };
 
     if (title.length && author.length) {
       dispatch(addBook(book));
-      setTitle('');
-      setAuthor('');
+      swal("Done!", `${title} successfully added`, "success");
+      setTitle("");
+      setAuthor("");
     }
   };
 
   return (
     <div>
       <h3 className="addBookHeader">ADD NEW BOOK</h3>
-      <form className="form-container">
+      <form onSubmit={handleSubmit} className="form-container">
         <input
+          className="title"
           type="text"
           placeholder="Add title..."
           value={title}
@@ -38,6 +40,7 @@ function InputBooks() {
           required
         />
         <input
+          className="author"
           type="text"
           placeholder="Add Author's name..."
           value={author}
@@ -45,7 +48,7 @@ function InputBooks() {
           onChange={(e) => setAuthor(e.target.value)}
           required
         />
-        <button type="button" onClick={handleSubmit}>
+        <button type="submit" onSubmit={handleSubmit} className="addBook">
           ADD BOOK
         </button>
       </form>
